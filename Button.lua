90b2d3c9bc98f744192b9ac6857925b57907bc16
Validark <NarrevRBX@gmail.com>
2018-01-16 21:26:48 -0700

    Update Button.lua

--- Button.lua
+++ Button.lua
@@ -1,4 +1,4 @@
--- Material Design Button element
+-- Material Design Button
 -- @author Validark
 
 -- Import Tween Library
@@ -23,13 +23,14 @@ FlatButton.BackgroundTransparency = 1
 FlatButton.ClipsDescendants = true
 FlatButton.Name = "FlatButton"
 
-local Corner = Instance.new("ImageLabel", FlatButton)
+local Corner = Instance.new("ImageLabel")
 Corner.BackgroundTransparency = 1
 Corner.Image = "rbxassetid://550542844"
 Corner.Name = "Corner"
 Corner.ScaleType = Enum.ScaleType.Slice
 Corner.SliceCenter = Rect.new(7, 7, 14, 14)
 Corner.Size = UDim2.new(1, 0, 1, 0)
+Corner.Parent = FlatButton
 
 local Circle = Instance.new("ImageLabel")
 Circle.AnchorPoint = Vector2.new(0.5, 0.5)
@@ -43,43 +44,80 @@ Circle.ZIndex = 7
 -- Preload Ink Ripple
 game:GetService("ContentProvider"):Preload(Circle.Image)
 
+-- Metamethods
+local function __newindex(self, i, v)
+	local Button = self.Button
+	local Corner = Button.Corner
+	if v then
+		if i == "Parent" and v:IsA("GuiObject") then
+			Corner.ImageColor3 = v.BackgroundColor3
+			Button.ZIndex = v.ZIndex + 1
+			Corner.ZIndex = v.ZIndex + 3
+			self.Connection[1]:Disconnect()
+			self.Connection[1] = v:GetPropertyChangedSignal("BackgroundColor3"):Connect(function()
+				Corner.ImageColor3 = v.BackgroundColor3
+			end)
+		elseif i == "TextColor3" then
+			Corner.BackgroundColor3 = v
+		end
+	end
+	Button[i] = v
+end
+
+local function __namecall(self, ...)
+	-- Not sure that this is the best way to do this
+	local Button = self.Button
+	local Arguments = {...}
+	local Method = table.remove(Arguments) -- select(-1, ...)
+	if Method == "Destroy" then
+		-- Do cleanup operations here
+		self.Connection[1] = self.Connection[1]:Disconnect()
+	elseif Method == "Ripple" then
+		self.Down{
+			UserInputType = Enum.UserInputType.MouseButton1;
+			Position = {
+				X = 0.5*Button.AbsoluteSize.X + Button.AbsolutePosition.X;
+				Y = 0.5*Button.AbsoluteSize.Y + Button.AbsolutePosition.Y;
+			}
+		}
+
+		wait(0.15)
+
+		return self.Up()
+	end
+
+	return Button(Method, unpack(Arguments)) -- Button[Method](Button, unpack(Arguments))
+end
+
+-- API
 local Button = {}
 
 function Button.new(Type, Parent, Theme)
-	local Button, Corner, CurrentCircle
+	-- Globals
+	local Button, Corner, CurrentCircle, Down, Up, CornerBackgroundTransparency
+	local Connection = {{Disconnect = function() end}}
+
+	-- Pseudo Object
 	local Interactable = newproxy(true)
 	local Metatable = getmetatable(Interactable)
-	local CornerBackgroundTransparency
+	Metatable.__newindex = __newindex
+	Metatable.__namecall = __namecall
 
-	function Metatable:__newindex(i, v)
-		if v then
-			if i == "Parent" and v:IsA("GuiObject") then
-				Corner.ImageColor3 = v.BackgroundColor3
-				Button.ZIndex = v.ZIndex + 1
-				Corner.ZIndex = v.ZIndex + 3
-				Button.Parent = v
-			elseif i == "TextColor3" then
-				Corner.BackgroundColor3 = v
-			end
+	function Metatable:__index(i)
+		if i == "Button" then
+			return Button
+		elseif i == "Down" then
+			return Down
+		elseif i == "Up" then
+			return Up
+		elseif i == "Connection" then
+			return Connection
+		else
+			return Button[i]
 		end
-		Tween(Button, i, v, "Deceleration", 0.35, true)
-		-- Button[i] = v
-	end
+	end	
 
-	function Metatable:__namecall(...)
-		-- Not sure that this is the best way to do this
-		local Arguments = {...}
-		local Method = table.remove(Arguments) -- select(-1, ...)
-		if Method == "Destroy" then
-			-- Do cleanup operations here
-			-- All of our connections our automatically Disconnected though via the `Destroy` method
-			-- Sooo... What do we do?
-		end
-
-		return Button(Method, unpack(Arguments)) -- Button[Method](Button, unpack(Arguments))
-	end
-
-	if Type == "Flat" then
+	if Type == "Flat" or Type == "Custom" then
 		Button = FlatButton:Clone()
 		Corner = Button.Corner
 
@@ -91,20 +129,19 @@ function Button.new(Type, Parent, Theme)
 		end
 		
 		CornerBackgroundTransparency = 0.88
+
+		if Type == "Custom" then
+			Corner.ImageTransparency = 1
+		end
 	elseif Type == "Raised" then
 		CornerBackgroundTransparency = 0.6
 		error("[Button] Invalid Button Type; not yet implemented")
-	elseif Type == "Custom" then
-		error("[Button] Invalid Button Type; not yet implemented")
 	else
-		error("[Button] Invalid Button Type; expected \"Flat\"")
+		error("[Button] Invalid Button Type; expected \"Flat\" or \"Custom\"")
 	end
 
-	Metatable.__index = Button
-
-	Button.InputBegan:Connect(function(InputObject)
+	function Down(InputObject)
 		if InputObject.UserInputType == MouseMovement then
-			Corner.BackgroundColor3 = Button.TextColor3
 			Tween(Corner, "BackgroundTransparency", CornerBackgroundTransparency, "Standard", 0.35, true)
 		elseif ValidInputEnums[InputObject.UserInputType] then
 			local PreviousCircle = CurrentCircle
@@ -128,17 +165,20 @@ function Button.new(Type, Parent, Theme)
 
 			Tween(Circle, "Size", UDim2.new(0, Diameter, 0, Diameter), "Deceleration", 0.5)
 		end
-	end)
+	end
 
-	Button.InputEnded:Connect(function(InputObject)
-		if InputObject.UserInputType == MouseMovement then
+	function Up(InputObject)
+		if InputObject and InputObject.UserInputType == MouseMovement then
 			Tween(Corner, "BackgroundTransparency", 1, "Standard", 0.35, true)
 		end
 		local CurrentCircle = CurrentCircle
 		if CurrentCircle then
 			Tween(CurrentCircle, "ImageTransparency", 1, "Deceleration", 1, false, true)
 		end
-	end)
+	end
+
+	Button.InputBegan:Connect(Down)
+	Button.InputEnded:Connect(Up)
 
 	return Interactable
 end
