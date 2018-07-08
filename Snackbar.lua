3ed42a6ba423eec93bd88c89056ffc180ffc11a6
howmanysmall <9477871+1@iechs.org>
2018-07-08 15:16:51 -0600

    Update Snackbar.lua
    Added the option to put in a Position using the Enumeration library.

--- Snackbar.lua
+++ Snackbar.lua
@@ -1,5 +1,7 @@
 -- Simple Snackbar Generator
 -- @readme https://github.com/RoStrap/UI/blob/master/README.md
+-- @documentation https://rostrap.github.io/Libraries/Material/Snackbar/
+-- @rostrap Snackbar
 -- @author Validark
 
 -- Snackbar.new(string Text, ScreenGui Screen)
@@ -14,29 +16,27 @@ local SMALLEST_WIDTH = 294
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Resources = require(ReplicatedStorage:WaitForChild("Resources"))
-
+local Enumeration = Resources:LoadLibrary("Enumeration")
 local Maid = Resources:LoadLibrary("Maid")
 local Tween = Resources:LoadLibrary("Tween")
+
+Enumeration.SnackbarPositions = {"Left", "Right", "Center"}
 
 local TweenCompleted = Enum.TweenStatus.Completed
 
 HEIGHT = HEIGHT + 6
 local OpenSnackbar, OpenTween
-local ExitPosition = UDim2.new(0.5, 0, 1, 0)
-local EnterPosition = UDim2.new(0.5, 0, 1, -HEIGHT)
 
 local DefaultSnackbar = Instance.new("ImageLabel")
-DefaultSnackbar.AnchorPoint = Vector2.new(0.5, 0)
 DefaultSnackbar.BackgroundTransparency = 1
 DefaultSnackbar.Image = "rbxasset://textures/ui/btn_newWhite.png"
 DefaultSnackbar.ImageColor3 = Color3.fromRGB(50, 50, 50)
-DefaultSnackbar.Position = UDim2.new(0.5, 0, 1, 0)
 DefaultSnackbar.ScaleType = Enum.ScaleType.Slice
 DefaultSnackbar.Size = UDim2.new(0, 288 + 6, 0, HEIGHT)
 DefaultSnackbar.SliceCenter = Rect.new(7, 7, 13, 13)
 DefaultSnackbar.ZIndex = 4
 
-local SnackbarText = Instance.new("TextLabel", DefaultSnackbar)
+local SnackbarText = Instance.new("TextLabel")
 SnackbarText.AnchorPoint = Vector2.new(0, 0.5)
 SnackbarText.Font = Enum.Font.SourceSans
 SnackbarText.Name = "Label"
@@ -45,12 +45,45 @@ SnackbarText.TextSize = 20
 SnackbarText.TextColor3 = Color3.fromRGB(255, 255, 255)
 SnackbarText.TextXAlignment = Enum.TextXAlignment.Left
 SnackbarText.ZIndex = 5
+SnackbarText.Parent = DefaultSnackbar
 
 local Snackbar = {}
+local PositionEnums = Enumeration.SnackbarPositions:GetEnumerationItems()
 
-function Snackbar.new(Text, Screen)
+function Snackbar.new(Text, Screen, Position)
 	-- @param string Text the message you want to appear
 	-- @param ScreenGui Screen the Parent of the Snackbar
+	-- @param Enumeration Position the positioning of the Snackbar
+	
+	local ExitPosition
+	local EnterPosition
+	
+	local PositionType = type(Position)
+	if PositionType == "string" then
+		Position = Enumeration.SnackbarPositions[Position].Name
+	elseif PositionType == "number" then
+		Position = PositionEnums[Position + 1].Name
+	elseif PositionType == "userdata" then
+		Position = Position.Name
+	elseif PositionType == "nil" then
+		Position = PositionEnums[2].Name
+	end
+	
+	if Position == "Left" then
+		DefaultSnackbar.Position = UDim2.new(0, 0, 1, 0)
+		ExitPosition = UDim2.new(0, 0, 1, 0)
+		EnterPosition = UDim2.new(0, 0, 1, -HEIGHT)
+	elseif Position == "Right" then
+		DefaultSnackbar.AnchorPoint = Vector2.new(1, 0)
+		DefaultSnackbar.Position = UDim2.new(1, 0, 1, 0)
+		ExitPosition = UDim2.new(1, 0, 1, 0)
+		EnterPosition = UDim2.new(1, 0, 1, -HEIGHT)
+	elseif Position == "Center" then
+		DefaultSnackbar.AnchorPoint = Vector2.new(0.5, 0)
+		DefaultSnackbar.Position = UDim2.new(0.5, 0, 1, 0)
+		ExitPosition = UDim2.new(0.5, 0, 1, 0)
+		EnterPosition = UDim2.new(0.5, 0, 1, -HEIGHT)
+	end
 	
 	if OpenSnackbar then
 		local PreviousSnackbar = OpenSnackbar
@@ -66,7 +99,7 @@ function Snackbar.new(Text, Screen)
 		OpenTween:Stop()
 		PreviousSnackbar.ZIndex = 2
 		
-		Tween(PreviousSnackbar, "Position", ExitPosition, "Acceleration", ENTER_TIME * 0.7, false, function(Completed)
+		Tween(PreviousSnackbar, "Position", ExitPosition, 2, ENTER_TIME * 0.7, false, function(Completed)
 			if Completed == TweenCompleted then
 				PreviousSnackbar:Destroy()
 				if OpenSnackbar == PreviousSnackbar then
@@ -91,9 +124,9 @@ function Snackbar.new(Text, Screen)
 
 	SnackbarFrame.Parent = Screen
 
-	OpenTween = Tween(SnackbarFrame, "Position", EnterPosition, "Deceleration", ENTER_TIME, false, function(Completed)
+	OpenTween = Tween(SnackbarFrame, "Position", EnterPosition, 1, ENTER_TIME, false, function(Completed)
 		if Completed == TweenCompleted and wait(DISPLAY_TIME - ENTER_TIME) then
-			Tween(SnackbarFrame, "Position", ExitPosition, "Acceleration", ENTER_TIME, false, function(Completed)
+			Tween(SnackbarFrame, "Position", ExitPosition, 2, ENTER_TIME, false, function(Completed)
 				if Completed == TweenCompleted then
 					SnackbarFrame:Destroy()
 					if OpenSnackbar == SnackbarFrame then
