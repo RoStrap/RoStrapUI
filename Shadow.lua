0f9c341caf4d14a04398c8be7c990b65770a0508
Validark <Validarkness@gmail.com>
2018-07-11 17:59:38 -0500

    Update Shadow.lua

--- Shadow.lua
+++ Shadow.lua
@@ -1,21 +1,37 @@
--- Simple Shadow Renderer
--- @readme https://github.com/RoStrap/UI/blob/master/README.md
+-- Shadow / Elevation Renderer PseudoInstance
+-- @documentation https://rostrap.github.io/Libraries/Material/Shadow/
 -- @author Validark
+-- @author AmaranthineCodices - Made the Shadow images and created the rendering framework
+-- @original https://github.com/AmaranthineCodices/roact-material/blob/master/src/Components/Shadow.lua
+
+local SHADOW_TWEEN_TIME = 0.175 --0.275
 
 local Resources = require(game:GetService("ReplicatedStorage"):WaitForChild("Resources"))
+local Debug = Resources:LoadLibrary("Debug")
 local Tween = Resources:LoadLibrary("Tween")
-
-local SHADOW_TWEEN_TIME = 0.275
+local Enumeration = Resources:LoadLibrary("Enumeration")
+local PseudoInstance = Resources:LoadLibrary("PseudoInstance")
 
 local ShadowImage = Instance.new("ImageLabel")
 ShadowImage.Image = "rbxassetid://1316045217"
 ShadowImage.ImageColor3 = Color3.fromRGB(0, 0, 0)
 ShadowImage.AnchorPoint = Vector2.new(0.5, 0.5)
 ShadowImage.BackgroundTransparency = 1
-ShadowImage.Position = UDim2.new(0.5, 0, 0.5, 0)
 ShadowImage.ScaleType = Enum.ScaleType.Slice
 ShadowImage.SliceCenter = Rect.new(10, 10, 118, 118)
-ShadowImage.Size = UDim2.new(1, 0, 1, 0)
+
+Enumeration.ShadowElevation = {
+	Elevation0 = 0;
+	Elevation1 = 1;
+	Elevation2 = 2;
+	Elevation3 = 3;
+	Elevation4 = 4;
+	Elevation6 = 6;
+	Elevation8 = 8;
+	Elevation9 = 9;
+	Elevation12 = 12;
+	Elevation16 = 16;
+}
 
 local ShadowData = {
 	[0] = {
@@ -216,47 +232,92 @@ local ShadowData = {
 
 for _, Elevation in next, ShadowData do
 	for _, Data in next, Elevation do
-		Data.Size, Data.Blur = UDim2.new(1, Data.Blur, 1, Data.Blur)
-		Data.Position, Data.Offset = UDim2.new(0.5, 0, 0.5, 0) + (Data.Offset or UDim2.new())
-		Data.ImageTransparency, Data.Opacity = 1 - Data.Opacity
+		Data.Size = UDim2.new(1, Data.Blur, 1, Data.Blur)
+		Data.Position = UDim2.new(0.5, 0, 0.5, 0) + (Data.Offset or UDim2.new())
+		Data.ImageTransparency = 1 - Data.Opacity
+
+		Data.Blur = nil
+		Data.Offset = nil
+		Data.Opacity = nil
 	end
 end
 
-local Shadow = {
-	Elevation = 3;
-}
-Shadow.__index = Shadow
+local ShadowNames = {"Ambient", "Penumbra", "Umbra"}
 
-function Shadow:ChangeElevation(Elevation)
-	if self.Elevation ~= Elevation then
-		for Name, Data in next, (assert(ShadowData[Elevation], "[Shadow] Non-existant elevation")) do
-			local Object = self[Name]
-			for Property, EndValue in next, Data do
-				Tween(Object, Property, EndValue, "Deceleration", SHADOW_TWEEN_TIME, true)
+return PseudoInstance:Register("Shadow", {
+	Properties = {
+		Elevation = function(self, Elevation)
+			Elevation = Enumeration.ShadowElevation:Cast(Elevation)
+
+			if self.Elevation ~= Elevation then
+				for Name, Data in next, ShadowData[Elevation.Value] do
+					local Object = self[Name]
+
+					for Property, EndValue in next, Data do
+						Object[Property] = EndValue
+					end
+				end
+				self:rawset("Elevation", Elevation)
 			end
+		end;
+
+		Parent = function(self, Parent)
+			local IsGuiObject = typeof(Parent) == "Instance" and Parent:IsA("GuiObject")
+
+			if IsGuiObject then
+				local function ZIndexChanged()
+					local ParentZIndex = Parent.ZIndex
+
+					for i = 1, 3 do
+						self[ShadowNames[i]].ZIndex = ParentZIndex - 1
+					end
+				end
+
+				self.Janitor:Add(Parent:GetPropertyChangedSignal("ZIndex"):Connect(ZIndexChanged), "Disconnect", "ZIndexChanged")
+				ZIndexChanged()
+
+				for i = 1, 3 do
+					self[ShadowNames[i]].Parent = Parent
+				end
+
+				return true
+			else
+				Debug.Error("bad argument 3 to Parent, expected GuiObject, got %s", Parent)
+			end
+		end;
+	};
+
+	Events = {};
+
+	Methods = {
+		ChangeElevation = function(self, Elevation)
+			Elevation = Enumeration.ShadowElevation:Cast(Elevation)
+
+			if self.Elevation ~= Elevation then
+				for Name, Data in next, ShadowData[Elevation.Value] do
+					local Object = self[Name]
+
+					for Property, EndValue in next, Data do
+						Tween(Object, Property, EndValue, "Deceleration", SHADOW_TWEEN_TIME, true)
+					end
+				end
+
+				self:rawset("Elevation", Elevation)
+			end
+		end;
+	};
+
+	Init = function(self)
+		for i = 1, 3 do
+			local Name = ShadowNames[i]
+			local Shadow = ShadowImage:Clone()
+			Shadow.Name = Name .. "Shadow"
+
+			self:rawset(Name, Shadow)
+			self.Janitor:Add(Shadow, "Destroy")
+			self.Janitor:LinkToInstance(Shadow, true)
 		end
-		self.Elevation = Elevation
-	end
-end
 
-function Shadow.new(Elevation, Parent)
-	local self = setmetatable({
-		Elevation = Elevation;
-	}, Shadow)
-
-	for Name, Properties in next, ShadowData[self.Elevation] do
-		local Shadow = ShadowImage:Clone()
-		Shadow.Name = Name .. "Shadow"
-		Shadow.Size = Properties.Size
-		Shadow.Position = Properties.Position
-		Shadow.ImageTransparency = Properties.ImageTransparency
-		Shadow.ZIndex = Parent.ZIndex - 1
-		Shadow.Parent = Parent
-
-		self[Name] = Shadow
-	end
-
-	return self
-end
-
-return Shadow
+		self:rawset("Elevation", Enumeration.ShadowElevation.Elevation0)
+	end;
+})
