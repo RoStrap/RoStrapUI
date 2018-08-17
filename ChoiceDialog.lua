2a9ae33f189be7941d7bb4eb1c17cbf1916f662d
Validark <Validarkness@gmail.com>
2018-08-17 16:13:54 -0500

    Update ChoiceDialog.lua

--- ChoiceDialog.lua
+++ ChoiceDialog.lua
@@ -10,6 +10,7 @@ local Resources = require(ReplicatedStorage:WaitForChild("Resources"))
 local Color = Resources:LoadLibrary("Color")
 local Debug = Resources:LoadLibrary("Debug")
 local Tween = Resources:LoadLibrary("Tween")
+local Typer = Resources:LoadLibrary("Typer")
 local Enumeration = Resources:LoadLibrary("Enumeration")
 local PseudoInstance = Resources:LoadLibrary("PseudoInstance")
 local ReplicatedPseudoInstance = Resources:LoadLibrary("ReplicatedPseudoInstance")
@@ -31,6 +32,13 @@ DialogBlur.Parent = Lighting
 local BUTTON_WIDTH_PADDING = 8
 local DISMISS_TIME = 75 / 1000 * 2
 local ENTER_TIME = 150 / 1000 * 2
+
+local Left = Enum.TextXAlignment.Left.Value
+local SourceSansSemibold = Enum.Font.SourceSansSemibold.Value
+
+local Flat = Enumeration.ButtonStyle.Flat.Value
+local InBack = Enumeration.EasingFunction.InBack.Value
+local OutBack = Enumeration.EasingFunction.OutBack.Value
 
 local LocalPlayer, PlayerGui do
 	if RunService:IsClient() then
@@ -63,12 +71,12 @@ local Frame do
 	Background.Parent = Frame
 
 	local Header = Instance.new("TextLabel")
-	Header.Font = Enum.Font.SourceSansSemibold
+	Header.Font = SourceSansSemibold
 	Header.TextSize = 26
 	Header.Size = UDim2.new(1, -24, 0, 64)
 	Header.Position = UDim2.new(0, 24, 0, 1)
 	Header.BackgroundTransparency = 1
-	Header.TextXAlignment = Enum.TextXAlignment.Left
+	Header.TextXAlignment = Left
 	Header.TextTransparency = 0.13
 	Header.TextColor3 = Color.Black
 	Header.Name = "Header"
@@ -133,15 +141,15 @@ return PseudoInstance:Register("ChoiceDialog", {
 			-- Destroys Dialog when done
 			if not self.Dismissed then
 				self.Dismissed = true
-				Tween(self.UIScale, "Scale", 0, Enumeration.EasingFunction.InBack, DISMISS_TIME, true, self.Janitor)
+				Tween(self.UIScale, "Scale", 0, InBack, DISMISS_TIME, true, self.Janitor)
 				self.UIScale.Parent = Screen
-				Tween(DialogBlur, "Size", 0, Enumeration.EasingFunction.InBack, ENTER_TIME, true)
+				Tween(DialogBlur, "Size", 0, InBack, ENTER_TIME, true)
 			end
 		end;
 	};
 
 	Properties = {
-		PrimaryColor3 = function(self, PrimaryColor3)
+		PrimaryColor3 = Typer.AssignSignature(2, Typer.Color3, function(self, PrimaryColor3)
 			self.ConfirmButton.PrimaryColor3 = PrimaryColor3
 			self.DismissButton.PrimaryColor3 = PrimaryColor3
 
@@ -150,10 +158,10 @@ return PseudoInstance:Register("ChoiceDialog", {
 				ItemContainer.PrimaryColor3 = PrimaryColor3
 			end
 
-			return true
-		end;
+			self:rawset("PrimaryColor3", PrimaryColor3)
+		end);
 
-		Options = function(self, Options)
+		Options = Typer.AssignSignature(2, Typer.ArrayOfStrings, function(self, Options)
 			local NumOptions = #Options
 			self.Background.Size = UDim2.new(0, 280, 0, 117 + 48 * NumOptions)
 
@@ -164,66 +172,62 @@ return PseudoInstance:Register("ChoiceDialog", {
 			end
 
 			for i = 1, NumOptions do
-				local Value = Options[i]
+				local ChoiceName = Options[i]
 
-				if type(Value) == "string" then
-					local ItemContainer = PseudoInstance.new("RippleButton")
-					ItemContainer.Position = UDim2.new(0, 0, 0, 64 + 48 * (i - 1))
-					ItemContainer.Size = UDim2.new(1, 0, 0, 48)
-					ItemContainer.BorderRadius = 0
-					ItemContainer.ZIndex = 5
-					ItemContainer.Style = Enumeration.ButtonStyle.Flat
-					ItemContainer.Parent = self.Background
+				local ItemContainer = PseudoInstance.new("RippleButton")
+				ItemContainer.Position = UDim2.new(0, 0, 0, 64 + 48 * (i - 1))
+				ItemContainer.Size = UDim2.new(1, 0, 0, 48)
+				ItemContainer.BorderRadius = 0
+				ItemContainer.ZIndex = 5
+				ItemContainer.Style = Flat
+				ItemContainer.Parent = self.Background
 
-					local Item = PseudoInstance.new("Radio")
-					Item.AnchorPoint = Vector2.new(0.5, 0.5)
-					Item.Position = UDim2.new(0, 36, 0.5, 0)
-					Item.ZIndex = 8
-					Item.Parent = ItemContainer.Object
+				local Item = PseudoInstance.new("Radio")
+				Item.AnchorPoint = Vector2.new(0.5, 0.5)
+				Item.Position = UDim2.new(0, 36, 0.5, 0)
+				Item.ZIndex = 8
+				Item.Parent = ItemContainer.Object
 
-					ItemContainer.PrimaryColor3 = self.PrimaryColor3
-					Item.PrimaryColor3 = self.PrimaryColor3
+				ItemContainer.PrimaryColor3 = self.PrimaryColor3
+				Item.PrimaryColor3 = self.PrimaryColor3
 
-					self.AssociatedRadioContainers[Item] = ItemContainer
-					self.RadioGroup:Add(Item, Value)
-					ItemContainer.OnPressed:Connect(Item.SetChecked, Item)
+				self.AssociatedRadioContainers[Item] = ItemContainer
+				self.RadioGroup:Add(Item, ChoiceName)
+				ItemContainer.OnPressed:Connect(Item.SetChecked, Item)
 
-					local ItemDescription = Instance.new("TextLabel")
-					ItemDescription.BackgroundTransparency = 1
-					ItemDescription.Position = UDim2.new(0, 48 + 32, 0.5, 0)
-					ItemDescription.TextXAlignment = Enum.TextXAlignment.Left
-					ItemDescription.Font = Enum.Font.SourceSansSemibold
-					ItemDescription.TextSize = 20
-					ItemDescription.Text = Value
-					ItemDescription.TextTransparency = 0.13
-					ItemDescription.ZIndex = 8
-					ItemDescription.Parent = ItemContainer.Object
-				else
-					Debug.Error("bad argument #3 to Options: Options must be an array of strings")
-				end
+				local ItemDescription = Instance.new("TextLabel")
+				ItemDescription.BackgroundTransparency = 1
+				ItemDescription.Position = UDim2.new(0, 48 + 32, 0.5, 0)
+				ItemDescription.TextXAlignment = Left
+				ItemDescription.Font = SourceSansSemibold
+				ItemDescription.TextSize = 20
+				ItemDescription.Text = ChoiceName
+				ItemDescription.TextTransparency = 0.13
+				ItemDescription.ZIndex = 8
+				ItemDescription.Parent = ItemContainer.Object
 			end
 
-			return true
-		end;
+			self:rawset("Options", Options)
+		end);
 
-		HeaderText = function(self, HeaderText)
-			self.Header.Text = HeaderText
-			return true
-		end;
+		HeaderText = Typer.AssignSignature(2, Typer.String, function(self, Text)
+			self.Header.Text = Text
+			self:rawset("HeaderText", self.Header.Text)
+		end);
 
-		DismissText = function(self, Text)
+		DismissText = Typer.AssignSignature(2, Typer.String, function(self, Text)
 			local DismissButton = self.DismissButton
 			DismissButton.Text = Text
 			DismissButton.Size = UDim2.new(0, DismissButton.TextBounds.X + BUTTON_WIDTH_PADDING * 2, 0, 36)
-			return true
-		end;
+			self:rawset("DismissText", DismissButton.Text)
+		end);
 
-		ConfirmText = function(self, Text)
+		ConfirmText = Typer.AssignSignature(2, Typer.String, function(self, Text)
 			local ConfirmButton = self.ConfirmButton
 			ConfirmButton.Text = Text
 			ConfirmButton.Size = UDim2.new(0, ConfirmButton.TextBounds.X + BUTTON_WIDTH_PADDING * 2, 0, 36)
-			return true
-		end;
+			self:rawset("ConfirmText", ConfirmButton.Text)
+		end);
 
 		Parent = function(self, Parent)
 			if Parent and PlayerGui then
@@ -233,11 +237,11 @@ return PseudoInstance:Register("ChoiceDialog", {
 				self.Object.Parent = Screen
 				self.DismissButton.Size = UDim2.new(0, self.DismissButton.TextBounds.X + BUTTON_WIDTH_PADDING * 2, 0, 36)
 				self.ConfirmButton.Size = UDim2.new(0, self.ConfirmButton.TextBounds.X + BUTTON_WIDTH_PADDING * 2, 0, 36)
-				Tween(self.UIScale, "Scale", 1, Enumeration.EasingFunction.OutBack, ENTER_TIME, true, HideUIScale, self)
-				Tween(DialogBlur, "Size", 56, Enumeration.EasingFunction.OutBack, ENTER_TIME, true)
+				Tween(self.UIScale, "Scale", 1, OutBack, ENTER_TIME, true, HideUIScale, self)
+				Tween(DialogBlur, "Size", 56, OutBack, ENTER_TIME, true)
 			end
 
-			return true
+			self:rawset("Parent", Parent)
 		end;
 	};
 
@@ -255,7 +259,7 @@ return PseudoInstance:Register("ChoiceDialog", {
 		ConfirmButton.ZIndex = 10
 		ConfirmButton.TextSize = 16
 		ConfirmButton.TextTransparency = 0.13
-		ConfirmButton.Style = Enumeration.ButtonStyle.Flat
+		ConfirmButton.Style = Flat
 		ConfirmButton.Parent = self.Background
 
 		local DismissButton = ConfirmButton:Clone()
